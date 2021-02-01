00b4d293c80947f1774892665bd2328669dd9fab
Sebastian <cruzguzmaneric@gmail.com>
2021-02-01 13:07:44 -0600

    Initial Commit

---
+++ Twitter-clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Twitter-clone
+//
+//  Created by PROGRAMAR on 01/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Twitter-clone/Twitter_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_cloneApp.swift
+//  Twitter-clone
+//
+//  Created by PROGRAMAR on 01/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
