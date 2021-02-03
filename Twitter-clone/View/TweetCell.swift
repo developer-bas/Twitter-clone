1483fba0b5edefd58a81dfeaf1431a7a75a35223
Sebastian <cruzguzmaneric@gmail.com>
2021-02-02 23:05:08 -0600

    Creating a scrollable list of tweets

---
+++ Twitter-clone/View/TweetCell.swift
@@ -0,0 +1,78 @@
+//
+//  TweetCell.swift
+//  Twitter-clone
+//
+//  Created by PROGRAMAR on 02/02/21.
+//
+
+import SwiftUI
+
+struct TweetCell: View {
+    var body: some View {
+        VStack{
+            HStack(alignment: .top){
+                Image("batman")
+                    .resizable()
+                    .scaledToFill()
+                    .clipped()
+                    .frame(width: 56, height: 56)
+                    .cornerRadius(56 / 2)
+                    .padding(.leading)
+                VStack(alignment:.leading, spacing: 4){
+                    HStack{
+                        Text("BRUCE WAYNE")
+                            .font(.system(size: 14, weight: .semibold))
+                        Text("@Batman  ")
+                            .foregroundColor(.gray)
+                        
+                        Text("2w")
+                            .foregroundColor(.gray)
+                        
+                        
+                        
+                    }
+                    Text("It´s not necesary use this thext but i wanna somo dummy data to show")
+                }
+            }
+            .padding(.bottom)
+            .padding(.trailing)
+            HStack{
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Image(systemName: "bubble.left")
+                        .font(.system(size: 16))
+                        .frame(width: 32, height: 32)
+                })
+                Spacer()
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Image(systemName: "arrow.2.squarepath")
+                        .font(.system(size: 16))
+                        .frame(width: 32, height: 32)
+                })
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Image(systemName: "heart")
+                        .font(.system(size: 16))
+                        .frame(width: 32, height: 32)
+                })
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Image(systemName: "bookmark")
+                        .font(.system(size: 16))
+                        .frame(width: 32, height: 32)
+                })
+                
+            }.padding(.horizontal)
+            .foregroundColor(.gray)
+            
+            Divider()
+        }
+    }
+}
+
+struct TweetCell_Previews: PreviewProvider {
+    static var previews: some View {
+        TweetCell()
+    }
+}
