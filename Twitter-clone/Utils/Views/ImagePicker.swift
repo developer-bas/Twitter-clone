57d243073695ff706eff5a7a89ec12d8b5e2d1f2
Sebastian <cruzguzmaneric@gmail.com>
2021-02-16 17:24:09 -0600

    Selecting profile image

--- Twitter-clone/Utils/Views/ImagePicker.swift
+++ Twitter-clone/Utils/Views/ImagePicker.swift
@@ -9,12 +9,17 @@ import UIKit
 import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable{
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) var mode
+    
+    
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
 
     func makeUIViewController(context: Context) -> some UIViewController {
         let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
         return picker
     }
     
@@ -34,6 +39,9 @@ extension ImagePicker{
         }
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             
+            guard let image = info[.originalImage] as? UIImage else {return}
+            parent.image = image
+            parent.mode.wrappedValue.dismiss()
         }
 
     }
