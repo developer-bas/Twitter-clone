078933d20f831b9b5b73076ff1a1358be29f8676
Sebastian <cruzguzmaneric@gmail.com>
2021-02-17 02:36:12 -0600

    Uploading user data to firestore

--- Twitter-clone/ViewModel/AuthViewModel.swift
+++ Twitter-clone/ViewModel/AuthViewModel.swift
@@ -16,13 +16,49 @@ class AuthViewModel: ObservableObject {
     }
     
     func registerUser(email:String,password: String,username: String,fullname: String, profileImage:UIImage){
-        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+        
+        
+        guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else {return}
+        
+        let filename = NSUUID().uuidString
+        let storegeRef = Storage.storage().reference().child(filename)
+        
+        storegeRef.putData(imageData, metadata: nil) { (_, error) in
             if let error = error {
-                print("DEBUG ERROR \(error.localizedDescription)")
+                print("DEBUG: Fail to upload image")
                 return
             }
-            print("Successfully signed user")
+            
+            storegeRef.downloadURL { (url, _) in
+                guard let profileImageURL = url?.absoluteString else {return}
+             
+                
+                
+                Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+                    if let error = error {
+                        print("DEBUG ERROR \(error.localizedDescription)")
+                        return
+                    }
+                    
+                    guard let user = result?.user else {return}
+                    
+                    let data = ["email":email,"username":username,"fullname":fullname, "profileImageUrl": profileImageURL,"uid": user.uid]
+                    
+                    Firestore.firestore().collection("users").document(user.uid).setData(data) { (_) in
+                        print("DEBUG: SUCCESSFULLY UPLOADED DATA")
+                    }
+                    
+                }
+                
+                
+            }
+            
         }
+        
+        
+        
+        
+   
     }
     
 }
