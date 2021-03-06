45e2ce127fdb457ff69993cf5bc4fef9103bf293
Sebastian <cruzguzmaneric@gmail.com>
2021-03-05 23:16:40 -0600

    git test

---
+++ Twitter-clone/Utils/Views/githubUIView.swift
@@ -0,0 +1,20 @@
+//
+//  githubUIView.swift
+//  Twitter-clone
+//
+//  Created by PROGRAMAR on 05/03/21.
+//
+
+import SwiftUI
+
+struct githubUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct githubUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        githubUIView()
+    }
+}
